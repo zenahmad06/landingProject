93fdfbd9b9d71efe158d2929f65603dc50f239cd
unknown <zenahmad06@gmail.com>
2025-08-16 16:43:10 -0700

    add content

--- src/Data/kumpul.js
+++ src/Data/kumpul.js
@@ -6,3 +6,11 @@ export const project_question = {
     link2:'https://nbviewer.org/github/zenahmad06/data/blob/main/Pertanyaan_Analitik.ipynb'
 
 }
+
+export const project_statistik = {
+    title:'Statistik dasar',
+    deskripsi:'Disini kita menggunakan beberapa statistik dasar dan implementasinya menggunakan python',
+    link1: 'https://medium.com/@zenahmad06/statistik-dasar-61c0343f16a6',
+    link2:'https://github.com/zenahmad06/data/blob/main/Statistik_dasar.ipynb'
+
+}
--- src/pages/DataPage.jsx
+++ src/pages/DataPage.jsx
@@ -1,10 +1,12 @@
 import CardItem from "../components/CardItem"
-import { project_question } from "../Data/kumpul"
+import { project_question,project_statistik } from "../Data/kumpul"
 export default function DataPage(){
     return(
         <>
         <div className="min-h-full w-full flex flex-row flex-wrap gap-5 px-2 py-2">
             <CardItem data={project_question}/>
+            <CardItem data={project_statistik}/>
+
         </div>
         </>
     )
