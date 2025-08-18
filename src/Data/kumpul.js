108b954e56a2ceed6f4c4253cb28f30fe8ef8481
unknown <zenahmad06@gmail.com>
2025-08-18 00:28:31 -0700

    add content

--- src/Data/kumpul.js
+++ src/Data/kumpul.js
@@ -1,7 +1,7 @@
 
 export const project_question = {
     title:'Pertanyaan analitik',
-    deskripsi:'Dalam latihan ini kita akan membangun pertanyann analitik dengan menggunakan contoh dataset',
+    deskripsi:'Dalam latihan ini saya akan membangun pertanyann analitik dengan menggunakan contoh dataset',
     link1: 'https://medium.com/@zenahmad06/pertanyaan-analitik-dc588a02968b',
     link2:'https://nbviewer.org/github/zenahmad06/data/blob/main/Pertanyaan_Analitik.ipynb'
 
@@ -9,8 +9,16 @@ export const project_question = {
 
 export const project_statistik = {
     title:'Statistik dasar',
-    deskripsi:'Disini kita menggunakan beberapa statistik dasar dan implementasinya menggunakan python',
+    deskripsi:'Disini saya menggunakan beberapa statistik dasar dan implementasinya menggunakan python',
     link1: 'https://medium.com/@zenahmad06/statistik-dasar-61c0343f16a6',
     link2:'https://nbviewer.org/github/zenahmad06/data/blob/main/Statistik_dasar.ipynb'
+
+
+}
+export const project_eda = {
+    title:'Explanatory Data Analysis',
+    deskripsi:'Dalam Project ini saya melakukan EDA dengan menggunakan churn dataset',
+    link1: 'https://medium.com/@zenahmad06/memulai-analisis-data-explanatory-data-analysis-3af992d33e73',
+    link2:'https://nbviewer.org/github/zenahmad06/data/blob/main/EDA_Customer_churn_visually.ipynb'
 
 }
--- src/pages/DataPage.jsx
+++ src/pages/DataPage.jsx
@@ -1,11 +1,13 @@
 import CardItem from "../components/CardItem"
-import { project_question,project_statistik } from "../Data/kumpul"
+import { project_question,project_statisti,project_eda } from "../Data/kumpul"
 export default function DataPage(){
     return(
         <>
         <div className="min-h-full w-full flex flex-row flex-wrap gap-5 px-2 py-2">
             <CardItem data={project_question}/>
             <CardItem data={project_statistik}/>
+            <CardItem data={project_eda}/>
+
 
         </div>
         </>
